7e3cb3ac4f93b2cbc94bf5128f861a6b600eb5f4
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-30 07:40:38 +0400

    finished the schemas for the graphql side and created the front end basic components

---
+++ client/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>Header</div>
+        );
+    }
+}
+
+export default Header;
--- server/server.js
+++ server/server.js
@@ -13,15 +13,21 @@ const MongoUri = require('../MongoUri');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = MongoUri();
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
-mongoose.connect(MONGO_URI);
-mongoose.connection
+mongoose.connect(MONGO_URI, {
+  authSource: "admin",
+  retryWrites: true,
+  dbName: "AuthSys",
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
+const db = mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
@@ -35,7 +41,7 @@ app.use(session({
   saveUninitialized: true,
   secret: 'aaabbbccc',
   store: new MongoStore({
-    url: MONGO_URI,
+    mongooseConnection: db,
     autoReconnect: true
   })
 }));
