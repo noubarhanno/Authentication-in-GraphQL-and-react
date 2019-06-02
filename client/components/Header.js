04a8159420def1b9b1f356dd03f7f0dd7cf01d9e
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-06-02 07:19:32 +0400

    Updated the mutation folder with logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,62 @@
 import React, {Component} from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
+import { Link } from 'react-router';
 
 class Header extends Component{
+    onLogoutClick(){
+        // in this.props you gonna see mutate function not mutation as it's imported in the very top of this file
+        // means even when i import mutation and i call it mutation this.props will contain the mutation function under "mutate"
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        });
+    }
+
+    renderButtons(){
+        const { loading, currentUser } = this.props.data;
+
+        if (loading){
+            return <div />;
+        }
+        if (currentUser){
+            return (
+            <li>
+                <a onClick={this.onLogoutClick.bind(this)}>
+                    Logout
+                </a>
+            </li>
+            );
+        } 
+        else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
+            );
+        }
+    }
+
     render(){
+        // when we run the query automatically the data will be included inside this.props.data
+        console.log(this.props.data)
         return(
-            <div>Header</div>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo left">Home</Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
 
-export default Header;
+// whenever we wrap the query by graphql helper method automatically when this component get rendered the query will be executed
+export default graphql(mutation)(graphql(query)(Header));
--- client/index.js
+++ client/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 
+
+// the code below is for 
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+// it is ApolloClient that is making actual request to the backend server
+// we can make how the request should be made to the backend by setuping up another option called networkInterface
+// we did this because when we call query to get the current user the graphql was recognizing that the user is authenticated but the front end was not 
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
