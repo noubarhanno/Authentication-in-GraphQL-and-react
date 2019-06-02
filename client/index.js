25b01b075f234663ffb94c48efa69f42c479eb7f
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-06-02 09:23:09 +0400

    Updated the AuthForm and LoginForm Frontend

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 
 // the code below is for 
@@ -27,7 +28,7 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-
+          <Route path="/login" component={LoginForm} />
         </Route>
       </Router>
     </ApolloProvider>
